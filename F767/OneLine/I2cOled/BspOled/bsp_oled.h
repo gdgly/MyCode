e65646855ac1aec2728bb95f4c420b854363bc05
yufei.fan <1490478564@qq.com>
2020-10-11 18:45:07 +0800

    图片显示

--- F767/OneLine/I2cOled/BspOled/bsp_oled.h
+++ F767/OneLine/I2cOled/BspOled/bsp_oled.h
@@ -16,11 +16,15 @@
 
 #define OLED0561_ADD    0x78
 
+extern unsigned char qt_image[1024];
+extern unsigned char nvidia_image[1024];
 
 void OLED_Init(void);
 void OLED_SetPos(unsigned char x, unsigned char y);
 void OLED_Fill(unsigned char fill_Data);
 void OLED_CLS(void);
+void OLED_DrawBMP(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, unsigned char BMP[]);
+
 
 #endif
 
